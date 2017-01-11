4037ad3b43aebe946644b62580118884b7c4765e
konefes <konefes@gmail.com>
2017-01-11 15:13:10 +0000

    timing again

--- app/models/timer_entry.rb
+++ app/models/timer_entry.rb
@@ -25,22 +25,31 @@ class TimerEntry < ActiveRecord::Base
     
     
     def year
-        ret = self.created_at #- 6.hours
+        ret = self.created_at + TimerEntry.time_diff.minutes #- 6.hours
         return ret.strftime('%Y')
     end
     
     def month
-        ret = self.created_at #- 6.hours
+        ret = self.created_at + TimerEntry.time_diff.minutes #- 6.hours
         return ret.strftime('%m')
     end
     
     def week
-        ret = self.created_at #- 6.hours
+        ret = self.created_at + TimerEntry.time_diff.minutes #- 6.hours
         return ret.strftime('%U')
     end
     
     def weekday
-        ret = self.created_at #- 6.hours
+        ret = self.created_at + TimerEntry.time_diff.minutes #- 6.hours
+        puts ret
         return ret.strftime('%w')
+    end
+    
+    def self.time_diff
+        current_time = Time.now
+        central_time = Time.now.in_time_zone("Central Time (US & Canada)")
+        time_diff = ((current_time - central_time) * 24 * 60).to_i
+        puts "time_diff: " + time_diff.to_s
+        return time_diff
     end
 end
